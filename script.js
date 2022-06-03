e083c6612670287a4e6e0ebb978b27c10c008bbc
ThroughEndurance <103070722+ThroughEndurance@users.noreply.github.com>
2022-06-03 15:33:22 -0600

    Add files via upload
    Cleaned up notes and formatting of JS. Added some additional CS for flavor, but could use more long term work on the aesthetics.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//WORKING NOTESdefine my constants here
-// "squareS" is not "square". not sure if the class name of just "square" would cause a conflict due to the class name.
-//FINAL NOTES: Constants should go here
+
+//Here I "found" the squares form the HTML using querySelectorAll. Defined the players (lizzo and dmx). Finally I build my restart function that wipes the board and the array that tracks the moves.
 const squares = document.querySelectorAll(".square")
 const lizzO = "O";
 const dmX = "X";
@@ -10,84 +9,41 @@ const restart = function(){
         square.innerText= '';
     })};
 
-// const allowed = (square) => {
-//     if (square.innerText === 'X' || square.innerText === 'O'){
-//         return false;
-//     }
-//     return true;
-// };
-
-
-//Final NOTES: Lets should go here
+//I set up my "let" variables. This includes the current player and I set up my array that tracks the moves.
 
 let currentPlayer = lizzO
 let board = ['', '', '', '', '', '', '', '', ''];
 
-
-//WORKING NOTES: This is the reset button. I need to have it reset the gameboard to blank. Right now it just console logs so we can confirm something happens
-//when it clicks.
-//info on button is in wordle recording around 33 min mark for placing button --- reset info is later hinted at but not
-//explaine fully.
-//we could maybe use textContent to clear out the squares??
-
-//FINAL NOTES:
-
-
+//This is the button's function. When clicked, it will run my reset function, which clears the tracking array in JS and resets all my squares visually in the HTML to ''
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(evt){
-    restart();    
-    console.log(evt)
+    restart();
+    console.log(board);
 });
 
-
-
-
-
-
-
-// WORKING NOTES: Use "this" within event listeners in order to target the specific element we want an X or O to get placed?? Maybe....
-//click listeners are explained in wordle recording at ~1:42.......... 
-// I'll try to use "event bubbling" principles to keep click listener code dry for the squares
-// const squares = document.querySelectorAll('div')   idk if I need this or not commented out temporarily
-
-//FINAL NOTES: Below function identifies where in the grid we clicked and adds an O to it.
+//The first part fo this code finds all my div's and gives them an event listener for clicking.
 
 document.querySelector('div').addEventListener('click', handleClick)
+
+//The next part of the code uses my handleClick event listener and identifies if my tracking array has a '' in it to allow the rest of the code to run.
+
 function handleClick(evt){
-    if(board[evt.target.id] === ''){
+    if(board[evt.target.id] === '')
+    
+//This part of the code changes the corresponding index of my array to the currenPlayer (an X or an O) using the target ID upon clicking on a square.     
+
+    {
     board[evt.target.id] = (currentPlayer);
 
+//This next section changes the text within the actual visual square to an X or an O using target.
 
     evt.target.innerText = currentPlayer
 
+//Finally upon click the current player changes back and forth between lizzO and dmX each time a square is clicked. I could get rid of the console log, however it will be potentially useful for icebox items I want to incorporate later on so I'm keeping it.
 
     if(currentPlayer === lizzO) currentPlayer = dmX
     else {
         currentPlayer = lizzO
     } 
     console.log(board)}}
-
-
-// currentPlayer = currentPlayer === lizzO ? dmX : lizzO; Tutor taught me this shorthand version but I could use
-//a deeper explaination of it. If/else statements written out the long way are much easier to read.
-
-
-//This below code does the same thing as the above code. Supposedly it is a better "real word" way of getting
-//the correct target due to how querySelector works. Above is how we did it in class in the wordle example so I'll try
-//to use the above code first, and resort to the bottom code if I run into errors. 
-//Note that the (x)=>handleClick(x) is essentially the same as just handleClick, handleClick is shorthand but limits
-//use of the event listener (or so we are told)
-
-// const squares = document.querySelectorAll(".square");
-// squares.forEach((square) => square.addEventListener("click", (x)=>handleClick(x)));
-// function handleClick(clickedSquare){
-//        console.log(clickedSquare.target.id)
-//        clickedSquare.target.innerText = lizzO
-// }
-
-
-//at this point I have my grid, reset button shell, and I can place alternating x's and o's. I now need to limit the 
-//placement to only empty squares, and finalize the reset button. 
-
-//bonus objectives are still needed.
